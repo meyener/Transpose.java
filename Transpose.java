ec48f6d27c689953b4c2bf8b185c5649eddf9e7b
M.Ekrem YENER <114063503+meyener@users.noreply.github.com>
2023-07-15 17:38:42 +0300

    Add files via upload

---
+++ Transpose.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Transpose {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println(Arrays.deepToString(transpose));
+    }
+}
